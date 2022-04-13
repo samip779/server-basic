53a665a434afae688803c5537afe91e430a0d00e
samip779 <samipluitel28@gmail.com>
2022-04-13 22:21:01 +0545

    user model created and basic server created with babel and webpack configuration

---
+++ server.js
@@ -0,0 +1,20 @@
+import config from "./config/config";
+import app from "./express";
+import mongoose from "mongoose";
+
+try {
+  mongoose.connect(
+    config.mongoUri,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log(" Mongoose is connected")
+  );
+} catch (e) {
+  console.log("could not connect");
+}
+
+app.listen(config.port, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.info(`Server started on port ${config.port}`);
+});
